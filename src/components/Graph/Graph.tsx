4fa4c4cc8eaadc2ed05de321ff0e54d393a000db
infidotte <99715439+infidotte@users.noreply.github.com>
2023-04-27 12:27:17 +0400

    Graph array inputs Fixed

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 // @ts-ignore
 import {Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, Title, BarElement} from 'chart.js';
 import {Bar, Pie} from 'react-chartjs-2';
 import {Stack} from "@fluentui/react";
-import {ITask} from "../../data/Types";
+import {TodoContext} from "../TodoProvider/TodoProvider";
 
 ChartJS.register(
     ArcElement,
@@ -15,66 +15,64 @@ ChartJS.register(
     Title,
     Tooltip,
     Legend);
-const Parse = (array: string | null) => {
-    if (array) {
-        return JSON.parse(array)
-    }
-}
-const active: ITask[] = Parse(localStorage.getItem('activeTasks'))
-const complete: ITask[] = Parse(localStorage.getItem('completedTasks'))
 
-const data = {
-
-    labels: ['Active', 'Completed'],
-    datasets: [
-        {
-            label: '# of Votes',
-            data: [active ? active.length : 0, complete ? complete.length : 0],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top' as const,
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Bar Chart',
-        },
-    },
-};
-const countPinned = () => {
-    let count = 0;
-    // eslint-disable-next-line array-callback-return
-    active.map(task => {
-        if (task.isPinned) {
-            count++;
-        }
-    })
-    return count;
-}
-export const barData = {
-    labels: ['Pinned', 'UnPinned'],
-    datasets: [
-        {
-            label: 'Dataset 1',
-            data: [countPinned(), active.length - countPinned()],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        }
-    ],
-};
 const Graph = () => {
+    const {
+        activeTasks,
+        completedTasks
+    } = useContext(TodoContext)
+
+    const data = {
+
+        labels: ['Active', 'Completed'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [activeTasks ? activeTasks.length : 0, completedTasks ? completedTasks.length : 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+    const countPinned = () => {
+        let count = 0;
+        // eslint-disable-next-line array-callback-return
+        activeTasks.map(task => {
+            if (task.isPinned) {
+                count++;
+            }
+        })
+        return count;
+    }
+    const barData = {
+        labels: ['Pinned', 'UnPinned'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [countPinned(), activeTasks.length - countPinned()],
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }
+        ],
+    };
     return <Stack>
         <Pie
             data={data}
